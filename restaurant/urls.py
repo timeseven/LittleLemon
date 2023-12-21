b20b3f52768feffb5b5be8a677d46d78b9ba33ad
Zhigeng Qian <evenwin88@gmail.com>
2023-12-21 16:43:35 +1100

    feat: add model and api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin 
-from django.urls import path 
+from django.contrib import admin
+from django.urls import path
 from . import views
-  
-urlpatterns = [ 
-    path('', views.index, name='index'), 
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    # add following lines to urlpatterns list
+    path('menu/', views.MenuItemsView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
