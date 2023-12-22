d3adc584f59e9331fd81e40ffb531efdf59991f0
Zhigeng Qian <evenwin88@gmail.com>
2023-12-22 18:50:48 +1100

    fix: urls

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,17 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from restaurant import views
-from rest_framework.authtoken.views import obtain_auth_token
 
-router = routers.SimpleRouter()
-router.register(r'tables', views.BookingViewSet)
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('restaurant/', include('restaurant.urls')),
-    path('restaurant/booking/', include(router.urls)),
+    path('', include('restaurant.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
-    path('api-token-auth/', obtain_auth_token)
+
 ]
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework import routers
 
+router = routers.SimpleRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     # add following lines to urlpatterns list
-    path('menu/', views.MenuItemsView.as_view()),
-    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('restaurant/menu/', views.MenuItemsView.as_view()),
+    path('restaurant/menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('restaurant/booking/', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token)
 ]
